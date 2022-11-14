902b86451a5c7ec3c7fc8960054311034c90874b
marouane <uanemaro216@gmail.com>
2022-11-14 19:44:27 +0100

    update repo

--- site web/admin/tmp/script.php
+++
@@ -1,5 +0,0 @@
-<script src="layout/js/main.js"></script>
-<script src="layout/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    
-</body>
-</html>
