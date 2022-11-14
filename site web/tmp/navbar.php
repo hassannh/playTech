f56bc4b0105932230da32d8785546ef304b4e138
marouane <uanemaro216@gmail.com>
2022-11-14 12:05:21 +0100

    navbar

--- site web/tmp/navbar.php
+++ site web/tmp/navbar.php
@@ -1,11 +1,14 @@
-<nav class="navbar navbar-expand-lg bg-light">
+<?php
+    include 'tmp/head.php';
+?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="home.php"><img src="layout/image/playtech-logo.png" alt="logo"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="home.php">Home</a>
         </li>
@@ -24,10 +27,14 @@
           </ul>
         </li>
       </ul>
-      <form class="d-flex" role="search">
+      <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
 </nav>
+
+<?php
+    include 'tmp/script.php';
+?>
