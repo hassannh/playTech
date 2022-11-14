a118f5c5eec15b1a186526f322e35548c8c93c29
hassannh <hassannouhi91@gmail.com>
2022-11-14 12:11:13 +0100

    update

--- site web/home.php
+++ site web/home.php
@@ -6,6 +6,45 @@
 
 
 
+<div>
+    <img class="backgrouandtech" src="layout/image/PT-Hero-Animated.gif" alt="gamesbackgrouand">
+</div>
+
+
+<div class="aboutU">
+    <div class="aboutU-img">
+        <img src="layout/image/home-block1.png" alt="storPic">
+    </div>
+    <div class="aboutU-desc">
+        <span><b>PlayPech</b></span>
+        <p>Playtech is a market leader
+             in the gambling and financial 
+             trading industries. We are the world's 
+             largest online gambling software supplier 
+             offering cutting-edge ...</p>
+
+    </div>
+</div>
+
+
+<div class="aboutU">
+    <div class="aboutU-desc">
+        <span>What Our Manager Said</span>
+        <p>Lorem, ipsum dolor sit amet
+             consectetur adipisicing elit.
+              Illum perferendis amet earum 
+              consequatur accusamus voluptatibus
+               illo at officia molestiae voluptatem
+                placeat cupiditate sit quia soluta explicabo
+                 rem aspernatur, consequuntur vero!</p>
+    </div>
+    <div class="aboutU-img">
+    <img src="layout/image/happy-senior-female-manager-portrait-office-37577449.jpg" alt="">
+    </div>
+</div>
+
+
+
 <?php
     include 'tmp/footer.php';
     include 'tmp/script.php';
