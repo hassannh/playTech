cffcba06e22c8248a0e8b6700c2dd05252342513
hassannh <hassannouhi91@gmail.com>
2022-11-17 12:06:20 +0100

    apdate

--- site web/connect.php
+++ site web/connect.php
@@ -1,15 +1,16 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=playtech';
+$myserver = 'mysql:host=localhost;dbname=playtech';
 $user = 'root';
 $pass = '';
 $option = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    //PDO (PHP Data Objects)
 );
 
 try {
     //check if data base there
-    $con = new PDO($dsn, $user, $pass, $option);
+    $con = new PDO($myserver, $user, $pass, $option);
     //connect data with data base
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
